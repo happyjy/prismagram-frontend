eb0a225136230823eff803d45edbcae598c1cbe7
happyjy <okwoyjy@gmail.com>
2019-11-20 22:14:45 +0900

    delete the alias of react-apollo-hooks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks';
+import { ApolloProvider } from 'react-apollo-hooks';
 import App from './Components/App';
 import Client from './Apollo/Client';
 
 ReactDOM.render(
-  <ApolloHooksProvider client={Client}>
+  <ApolloProvider client={Client}>
     <App />
-  </ApolloHooksProvider>,
+  </ApolloProvider>,
   document.getElementById('root'),
 );
