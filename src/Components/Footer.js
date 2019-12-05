5811058db37cfa8da9b88181b25c432548748aa1
happyjy <okwoyjy@gmail.com>
2019-12-05 22:52:32 +0900

    #7.0 Getting the Feed and Loader Component

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,7 +8,8 @@ const Footer = styled.footer`
     align-items: center;
     text-transform: uppercase;
     font-weight: 600
-    font-size 12px`
+    font-size 12px
+    margin 50px`
 
 const List  = styled.ul`
     display: flex
