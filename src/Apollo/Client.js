4928c9db7876c690c579bac5d08cb82a6007c35b
happyjy <okwoyjy@gmail.com>
2020-07-01 22:25:16 +0900

    add real uri

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -2,12 +2,15 @@ import ApolloClient from 'apollo-boost';
 import { defaults, resolvers } from './LocalState';
 
 export default new ApolloClient({
-  uri: 'http://localhost:4000', //graphql endpoint
-  clientState: {
-    defaults,
-    resolvers,
-  },
-  headers: {
-    Authorization: `Bearer ${localStorage.getItem('token')}`,
-  },
+	uri:
+		process.env.NODE_ENV === 'development'
+			? 'http://localhost:4000'
+			: 'https://jyooninstagram-backend.herokuapp.com/',
+	clientState: {
+		defaults,
+		resolvers
+	},
+	headers: {
+		Authorization: `Bearer ${localStorage.getItem('token')}`
+	}
 });
